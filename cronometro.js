aefa05f413e06c0464b26d7d4b43a7a96c5fdd51
DevTinel <davidaugustolol36@gmail.com>
2022-06-11 22:31:03 -0300

    u

---
+++ cronometro.js
@@ -0,0 +1,43 @@
+const tempo = document.querySelector(".tempo");
+const iniciar = document.querySelector("[data-inicio]");
+const pause = document.querySelector("[data-pause]");
+const reset = document.querySelector("[data-reset]");
+const minutos = document.querySelector(".min");
+
+iniciar.addEventListener("click", inicio);
+pause.addEventListener("click", pararTempo);
+reset.addEventListener("click", resetarTempo);
+
+let sec = 00;
+let min = 00;
+let hr = 00;
+let timer;
+function inicio() {
+  timer = setInterval(() => {
+    tempo.innerHTML = hr + ":" + min + ":" + sec++;
+    if (sec === 60) {
+      sec = 0;
+      min++;
+      if (min === 60) {
+        min = 0;
+        hr++;
+      }
+      if (min < 10) min = "0" + min;
+    }
+  }, 10);
+  iniciar.setAttribute("disabled", ""); //desabilita o clicar enqnt o tempo ta rodando
+}
+
+function pararTempo() {
+  clearInterval(timer);
+  iniciar.removeAttribute("disabled"); //poder clicar no iniciar
+}
+
+function resetarTempo() {
+  setTimeout(() => {
+    tempo.innerHTML = "00:00:00";
+    min = 0;
+    sec = 0;
+    hr = 0;
+  });
+}
